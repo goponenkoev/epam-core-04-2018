f752df87fc22b1ea4c8738b2055ea80dd7f963eb
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-05-02 13:36:11 +0300

    First commit

--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task13 {
 
     /**
@@ -51,5 +53,45 @@ public class Task13 {
      */
     public static void main(String[] args) {
         // TODO реализация
+        try(Scanner scanner  = new Scanner(System.in)){
+            int[][] matrix = readMatrix(scanner);
+            printMatrix(shiftRows(matrix, scanner.nextInt()));
+        }
+    }
+
+    private static int[][] shiftRows(int[][] matrix, int k) {
+        List<int[]> list = new LinkedList<>(Arrays.asList(matrix));
+        for (int i = 0; i < Math.abs(k); i++) {
+            if (k < 0){
+                int[] temp = list.remove(0);
+                list.add(temp);
+            }else{
+                int[] temp = list.remove(list.size() - 1);
+                list.add(0, temp);
+            }
+        }
+        matrix =  list.toArray(new int[list.size()][]);
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        for (int[] aMatrix : matrix) {
+            for (int col = 0; col < matrix.length; ++col) {
+                System.out.print(aMatrix[col] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
