861fe67bc560f14c917447bff5ab5ab9c1160d32
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-06-14 01:12:50 +0300

    First commit

---
+++ src/main/java/com/epam/homework/task23/Task23Impl.java
@@ -0,0 +1,20 @@
+package com.epam.homework.task23;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task23Impl implements Task23 {
+    @Override
+    public Set<Integer> intersection(Set<Integer> first, Set<Integer> second) {
+        HashSet<Integer> resultSet = new HashSet<>(first);
+        resultSet.retainAll(second);
+        return resultSet;
+    }
+
+    @Override
+    public Set<Integer> union(Set<Integer> first, Set<Integer> second) {
+        HashSet<Integer> resultSet = new HashSet<>(first);
+        resultSet.addAll(second);
+        return resultSet;
+    }
+}
