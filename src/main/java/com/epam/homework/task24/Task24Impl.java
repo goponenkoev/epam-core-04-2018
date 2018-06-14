815789b15a73983f4e0404479e2e9ad5060628ec
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-06-14 12:26:16 +0300

    Some fixes

--- src/main/java/com/epam/homework/task24/Task24Impl.java
+++ src/main/java/com/epam/homework/task24/Task24Impl.java
@@ -6,14 +6,14 @@ import java.util.Map;
 public class Task24Impl implements Task24{
     @Override
     public Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second) {
+
         Map<Integer, Integer> resultMap = new HashMap<>(first);
 
         for (Map.Entry cur: second.entrySet()){
-            resultMap.computeIfAbsent((Integer)cur.getKey(), (val) -> (Integer)cur.getValue());
             resultMap.computeIfPresent((Integer)cur.getKey(), (key, val) -> (Integer)cur.getValue() + val);
+            resultMap.computeIfAbsent((Integer)cur.getKey(), (val) -> (Integer)cur.getValue());
         }
 
         return resultMap;
     }
-
 }
