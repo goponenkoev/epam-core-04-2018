bffbad6ad6998d182473e659448b12c87a98fc8f
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-06-22 01:39:11 +0300

    First commit

---
+++ src/main/java/com/epam/homework/task28/Task28Impl.java
@@ -0,0 +1,57 @@
+package com.epam.homework.task28;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Task28Impl implements Task28 {
+    @Override
+    public int getNumberOvertaking(Set<Car> cars, int lapLength, int lapsCount) {
+        List<Car> listCars = new ArrayList<>(cars);
+        int distance = lapLength * lapsCount;
+        int overTakingCount = 0;
+
+        for (int i = 0; i < listCars.size(); i++) {
+            for (int j = i + 1; j < listCars.size(); j++) {
+                overTakingCount += countOverTaing(listCars.get(i), listCars.get(j), distance, lapLength);
+            }
+        }
+        return overTakingCount;
+    }
+
+    private static int countOverTaing(Car firstCar, Car secondCar, int distance, int lapLength) {
+        int result = 0;
+        int timeFirstCar = distance / firstCar.getSpeed();
+        int timeSecondCar = distance / secondCar.getSpeed();
+        int difference = (distance - secondCar.getSpeed() * Math.min(timeFirstCar, timeSecondCar));
+
+        result += difference / lapLength;
+
+        if (!((secondCar.getSpeed() == firstCar.getSpeed()) && (firstCar.getStartPosition() == secondCar.getStartPosition()))) {
+            ++result;
+        }
+        return result;
+    }
+}
+
+class CarImpl implements Task28.Car {
+
+    private int position;
+    private int speed;
+
+    CarImpl(int p, int s) {
+        this.position = p;
+        this.speed = s;
+    }
+
+    @Override
+    public int getStartPosition() {
+        return position;
+    }
+
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+}
